918412fd02e6efe2be7dcdb9bb628ed7ac325d44
volpertinger <merzlovnik@mail.ru>
2022-07-13 17:43:19 +0300

    Initial commit

---
+++ teko_bot/Program.cs
@@ -0,0 +1,15 @@
+﻿using Telegram.Bot;
+
+namespace teko_bot
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("5446428708:AAEOFR_1BHWh0G6W8yCNzDgJkU7tSfBgS3g");
+
+            var me = await botClient.GetMeAsync();
+            Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+        }
+    }
+}
