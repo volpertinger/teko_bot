d3b7fec486b54cb6a9dcb19c5e8a807f18dca69a
volpertinger <merzlovnik@mail.ru>
2022-07-14 13:33:41 +0300

    Db frame developed

--- teko_bot/BotConfiguration.cs
+++ teko_bot/BotConfiguration.cs
@@ -1,7 +1,11 @@
 namespace teko_bot;
 
+using System.IO;
+
 public static class BotConfiguration
 {
     public const string BotToken = "5446428708:AAEOFR_1BHWh0G6W8yCNzDgJkU7tSfBgS3g";
     public const string BotName = "teko_test_bot";
+    public const string DbSource = "teko_bot.db";
+    public const string DbLogPath = "DbLog.txt";
 }
---
+++ teko_bot/Db.cs
@@ -0,0 +1,82 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Globalization;
+
+namespace teko_bot;
+
+public class Company
+{
+    public int CompanyId { get; set; }
+    public string Name;
+
+    public List<Bill> Bills { get; set; } = new();
+
+    public static async void addToDb(ApplicationContext db, string name)
+    {
+        db.Add(new Company { Name = name });
+        await db.SaveChangesAsync();
+    }
+}
+
+public class Bill
+{
+    public int BillId { get; set; }
+    public string? Description { get; set; }
+    public string Email { get; set; }
+    public string Date { get; set; }
+
+    // Связь 1 - много: Company - Bill
+    public int CompanyId { get; set; }
+    public Company? Company { get; set; }
+
+    public static async void addToDb(ApplicationContext db, string? description, string email, int companyId)
+    {
+        // чтобы не было ошибки при добавлении счета к несуществующей компании
+        var company = await db.Companies.FindAsync(companyId);
+        if (company is null)
+        {
+            return;
+        }
+
+        db.Add(new Bill
+        {
+            Description = description, Email = email, Date = DateTime.Now.ToString(CultureInfo.InvariantCulture),
+            CompanyId = companyId
+        });
+        await db.SaveChangesAsync();
+    }
+}
+
+public sealed class ApplicationContext : DbContext
+{
+    readonly StreamWriter logStream = new StreamWriter(BotConfiguration.DbLogPath, true);
+    public DbSet<Company> Companies => Set<Company>();
+    public DbSet<Bill> Bills => Set<Bill>();
+
+    public ApplicationContext()
+    {
+        // TODO: убрать очистку
+        // очистка для тестирования
+        Database.EnsureDeleted();
+        // гарантируем, что БД создана и если нет, то создаем
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={BotConfiguration.DbSource}");
+        optionsBuilder.LogTo(logStream.WriteLine);
+    }
+
+    public override void Dispose()
+    {
+        base.Dispose();
+        logStream.Dispose();
+    }
+
+    public override async ValueTask DisposeAsync()
+    {
+        await base.DisposeAsync();
+        await logStream.DisposeAsync();
+    }
+}
